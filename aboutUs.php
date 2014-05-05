efce55e124d2d98f428f2dfbfdfeb1f65e90fa58
cwilli13 <ryanwilliams92@gmail.com>
2014-05-05 12:50:47 -0400

    Added content to aboutUs
    -Added additional styles to style.css

--- aboutUs.php
+++ aboutUs.php
@@ -9,10 +9,77 @@ include 'top.php';
     include 'nav.php';
     ?>
 <section id="main">
-	<h1>Welcome to Paddy's Pub</h1>
-        <p>
-            "The Most Freedom Loving Bar in America"
-        </p>
+    <div class="employee">
+
+    	<div class="employeeImage">
+            <img src="images/Charlie.jpg" alt="Charlie" />
+        </div>
+
+        <h2>Charlie Kelly</h2>
+
+        <p>Charlie was a co-owner at Paddy's pub, but traded all his stocks for "goods and services", 
+            half a sandwich, and other unnamed reasons.  Now Charlie is responsible for "Charlie Work". </p>
+
+    </div>
+
+    <div class="employee">
+
+        <div class="employeeImage">
+            <img src="images/dee1.jpg" alt="Dee" />
+        </div>
+
+        <h2>Deandra "Sweet Dee" Reynolds</h2>
+
+        <p>Dee is the main bartender at Paddy's Pub.</p>
+
+    </div>
+
+    <div class="employee">
+
+        <div class="employeeImage">
+            <img src="images/dennis1.jpg" alt="Dennis" />
+        </div>
+
+        <h2>Dennis Reynolds</h2>
+
+        <p>Dennis is a co-owner and occasional bartender of Paddy's Pub.  </p>
+
+    </div>
+
+    <div class="employee">
+
+        
+        <div class="employeeImage">
+            <img src="images/mac1.jpg" alt="Mac" />
+        </div>
+
+        <h2>Ronald "Mac" McDonald</h2>
+
+        <p>Mac is a co-owner of Paddy's Pub and is responsible for security as the "sheriff of Paddy's".</p>
+
+    </div>
+
+    <div class="employee">
+
+        <div class="employeeImage">
+            <img src="images/frank.png" alt="Frank" />
+        </div>
+
+        <h2>Frank Reynolds</h2>
+
+        <p class ="employeeBlurb">Frank is Dennis and Dee's father and the Landlord of Paddy's Pub.  He uses his fatherly instinct and experience
+            to help manage the pub.</p>
+
+    </div>
+
+
+   
+
+    
+
+
+    
+
 </section>
 
     <!--<section id='other'>
