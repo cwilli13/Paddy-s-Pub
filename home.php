671083a3661952910e713a2aef9b429931dda33e
Josh <jmlane@uvm.edu>
2014-05-05 11:36:03 -0400

    changing php title function

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+include 'top.php?title=main';
+?>
+
+<body>
+    <?php 
+    include 'header.php';
+    include 'nav.php';
+    ?>
+    <div class="wrapper">
+  	<h2>Web Usability Test</h2>
+	<a class='logo' href="http://www.leonardosonline.com/" target="_blank"><img src="leo-logo.gif" alt="Leonardo's Logo"/></a>
+	<a class='script' href="script.pdf" target="_blank">Script & Questions</a>
+    </div>
+  </body>
+</html>
--- top.php
+++ top.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Paddy's Pub</title>
+    
+<?php
+$pageName=$_GET["title"];
+?>
+<title>
+    <?php
+    print "$pageName";
+    ?>
+</title>
+
+
+
 <meta charset="utf-8">
 <meta name="author" content="Josh and Ryan">
 <meta name="description" content="Assignment 7 - Final Project">
