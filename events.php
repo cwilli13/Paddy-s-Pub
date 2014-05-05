62cb3e70667ee15f1910b2f3e85a0761bf668c40
Josh <jmlane@uvm.edu>
2014-05-05 12:57:31 -0400

    style changes

--- events.php
+++ events.php
@@ -18,7 +18,7 @@ include 'top.php';
                 <span class='date'>May 8</span> Half price wings and soup!
             </li>
             <li>
-                <span class='date'>May 15</span> Tap Takeover! <a href="http://www.tiredhands.com">Tired Hands</a> brewery will be bringing 4 kegs for us to tap,  HopHands, Wilbur, Sunulate, and Third Level.
+                <span class='date'>May 15</span> Tap Takeover! <a href="http://www.tiredhands.com">Tired Hands</a> brewery will be bringing 4 kegs for us to tap;  HopHands, Wilbur, Sunulate, and Third Level.
             </li>
             <li>
                 <span class='date'>May 22</span> $3.50 Craft beers
