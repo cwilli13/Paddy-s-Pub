efd04b139a8986259f9766b5154e4b33083cb192
cwilli13 <ryanwilliams92@gmail.com>
2014-05-05 12:14:23 -0400

    added additional pages
    deleted master.css
    created templates for additional pages
    -aboutUs
    -contactUs
    -events
    -menu

---
+++ contactUs.php
@@ -0,0 +1,27 @@
+<?php
+$title='Contact Us';
+include 'top.php';
+?>
+
+<body>
+    <?php 
+    include 'header.php';
+    include 'nav.php';
+    ?>
+<section id="main">
+	<h1>Welcome to Paddy's Pub</h1>
+        <p>
+            "The Most Freedom Loving Bar in America"
+        </p>
+</section>
+
+    <!--<section id='other'>
+            <img id='frontimage' src="chinookerd.jpg" width="257" height="400" alt="Chinooker'd">
+    </section>
+-->
+<?
+include ("footer.php");
+?>
+
+</body>
+</html>
