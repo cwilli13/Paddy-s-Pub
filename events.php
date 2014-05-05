55d2d88c21f7e3cb442785a566a344b8100812e1
Josh <jmlane@uvm.edu>
2014-05-05 12:52:50 -0400

    added events

--- events.php
+++ events.php
@@ -8,11 +8,26 @@ include 'top.php';
     include 'header.php';
     include 'nav.php';
     ?>
-<section id="main">
-	<h1>Welcome to Paddy's Pub</h1>
+<section id="events">
+	<h1>Upcoming Events</h1>
         <p>
-            "The Most Freedom Loving Bar in America"
+            We have an event every Thursday! All events begin at 5pm and last until close (3am).
         </p>
+        <ul>
+            <li>
+                <span class='date'>May 8</span> Half price wings and soup!
+            </li>
+            <li>
+                <span class='date'>May 15</span> Tap Takeover! <a href="http://www.tiredhands.com">Tired Hands</a> brewery will be bringing 4 kegs for us to tap,  HopHands, Wilbur, Sunulate, and Third Level.
+            </li>
+            <li>
+                <span class='date'>May 22</span> $3.50 Craft beers
+            </li>
+            <li>
+                <span class='date'>May 29</span> Karaoke Night! One free drink for each participant.
+            </li>
+        </ul>
+
 </section>
 
     <!--<section id='other'>
