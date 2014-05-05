e7b509ce3d898e4e3688c6bbaaee75425a0b1079
Josh <jmlane@uvm.edu>
2014-05-05 12:01:55 -0400

    added header

---
+++ header.php
@@ -0,0 +1,4 @@
+<header>
+    <h1><a href="home.php">Paddy's Pub</a></h1>
+    <h2>"The Most Freedom Loving Bar in America"</h2>
+</header>
--- nav.php
+++ nav.php
@@ -7,35 +7,24 @@ if(basename($_SERVER['PHP_SELF'])=="home.php"){
     print '<li><a href="home.php">Home</a></li>';
 } 
 
+if(basename($_SERVER['PHP_SELF'])=="menu.php"){
+    print '<li class="activePage">Menu</li>';
+} else {
+    print '<li><a href="menu.php">Menu</a></li>';
+} 
+
+if(basename($_SERVER['PHP_SELF'])=="events.php"){
+    print '<li class="activePage">Events</li>';
+} else {
+    print '<li><a href="events.php">Events</a></li>';
+}
+
+
 if(basename($_SERVER['PHP_SELF'])=="aboutUs.php"){
     print '<li class="activePage">About Us</li>';
 } else {
     print '<li><a href="aboutUs.php">About Us</a></li>';
 } 
-
-if(basename($_SERVER['PHP_SELF'])=="reputation.php"){
-    print '<li class="activePage">Reputation</li>';
-} else {
-    print '<li><a href="reputation.php">Reputation</a></li>';
-}
-
-if(basename($_SERVER['PHP_SELF'])=="productHistory.php"){
-    print '<li class="activePage">Product History</li>';
-} else {
-    print '<li><a href="productHistory.php">Product History</a></li>';
-} 
-
-if(basename($_SERVER['PHP_SELF'])=="validityAndReliability.php"){
-    print '<li class="activePage">Validity & Reliability</li>';
-} else {
-    print '<li><a href="validityAndReliability.php">Validity & Reliability</a></li>';
-} 
-
-if(basename($_SERVER['PHP_SELF'])=="publishedResearch.php"){
-    print '<li class="activePage">Published Research</li>';
-} else {
-    print '<li><a href="publishedResearch.php">Published Research</a></li>';
-}
 
 if(basename($_SERVER['PHP_SELF'])=="contactUs.php"){
     print '<li class="activePage">Contact Us</li>';
