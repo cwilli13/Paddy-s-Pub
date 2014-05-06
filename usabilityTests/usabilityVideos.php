f4600ddcc1b779ddb5b3e718e604e7612efa9e97
cwilli13 <ryanwilliams92@gmail.com>
2014-05-06 13:15:30 -0400

    Added Web Usability Test videos
    usabilityVideos.php

--- usabilityTests/usabilityVideos.php
+++ usabilityTests/usabilityVideos.php
@@ -13,12 +13,13 @@ include 'usabilityTop.php';
             <div>
                 
                 <iframe class="video" width="420" height="315" src="//www.youtube.com/embed/KWYS9ot_H30?rel=0" frameborder="0" allowfullscreen></iframe>
-            </div>
+                <iframe class="video" width="420" height="236" src="//www.youtube.com/embed/ZZHwTgYIUBk?rel=0" frameborder="0" allowfullscreen></iframe>
+             </div>
             <p>Round 2 of testing</p>
             <div>
                 
                 <iframe class="video" width="420" height="315" src="//www.youtube.com/embed/90iNMtBuNQk?rel=0" frameborder="0" allowfullscreen></iframe>
-
+                <iframe class="video" width="420" height="236" src="//www.youtube.com/embed/Y47ckFMebBw?rel=0" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
